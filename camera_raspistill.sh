1d63167829123cb56cad555ab75a699a389a5f6b
Ning Shang <syncom.dev@gmail.com>
2016-03-08 08:54:39 -0800

    Reduced quality from 50 to 15 in order to limit file size.

--- camera_raspistill.sh
+++ camera_raspistill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 DATE=$(date +"%Y-%m-%d_%H%M")
-raspistill --nopreview -w 640 -h 480 -q 50 -o /mnt/ramdisk/${DATE}.jpg
+raspistill --nopreview -w 640 -h 480 -q 15 -o /mnt/ramdisk/${DATE}.jpg
 
