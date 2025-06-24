c3a53c05e79cb5274603c575df9f2fbb03085353
Ning Shang <syncom.dev@gmail.com>
2025-06-24 16:39:43 -0700

    Improve twitimg-motion-run.sh

--- twitimg-motion-run.sh
+++ twitimg-motion-run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-ROOTDIR=`dirname $0`
+set -euxo pipefail
+
+SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 photo_dir="/mnt/ramdisk_motion"
-python ${ROOTDIR}/tweet_motion_jpg.py ${photo_dir}
+
+python3 -v venv "${SCRIPT_DIR}"/venv3
+# shellcheck source=/dev/null
+source "${SCRIPT_DIR}"/venv3/bin/activate
+python "${SCRIPT_DIR}"/tweet_motion_jpg.py "${photo_dir}"
