63361e11193a4f34ce7c946b543baf0a2dbc0417
Ning Shang <syncom.dev@gmail.com>
2025-06-24 21:20:48 -0700

    Bugfix: python venv
    Fix a bug in script.

--- twitimg-motion-run.sh
+++ twitimg-motion-run.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 photo_dir="/mnt/ramdisk_motion"
 
-python3 -v venv "${SCRIPT_DIR}"/venv3
+python3 -m venv "${SCRIPT_DIR}"/venv3
 # shellcheck source=/dev/null
 source "${SCRIPT_DIR}"/venv3/bin/activate
 pip install -r "${SCRIPT_DIR}"/requirements.txt
